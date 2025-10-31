52e847a90dbda386cbb3a0195da1786315f5c65f
TP-T4 <Tauri.Parli@tptlive.ee>
2025-10-31 10:30:37 +0200

    klassid ja andmebaas

---
+++ KooliProjekt.Application/Data/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KooliProjekt.Application.Data
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public int ToDoListId { get; set; }
+        public ToDoList ToDoList { get; set; }
+    }
+}
---
+++ KooliProjekt.Application/Data/ToDoList1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace KooliProjekt.Application.Data
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<ToDoItem> Items { get; set; } = new List<ToDoItem>();
+    }
+}
